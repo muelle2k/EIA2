ac9d90c78d4bd98d61fcd808ecf8075275dc2c2c
patrickmk <patrickmk@patricks-mbp-2.eduroam.hs-furtwangen.de>
2018-03-22 15:56:21 +0100

    Mini-Seite

---
+++ MiniSeite/js/miniseite.js
@@ -0,0 +1,8 @@
+function Greeter() {
+    var put = prompt("Ich kenne dich doch! Wie heisst du nochmal? ", "");
+    if (put != null) {
+        document.getElementById("Eingabefeld").innerHTML =
+            "Ach ja! Stimmt! " + put + " war dein Name.";
+    }
+}
+//# sourceMappingURL=miniseite.js.map
