cc4552ab21f8760fd871878ebbf1470c367c59b8
patrickmk <patrickmk@patricks-mbp-2>
2018-04-15 18:05:31 +0200

    Aufgabe 2 Memory

---
+++ Aufgabe2/js/game.js
@@ -0,0 +1,101 @@
+var memorySpiel;
+(function (memorySpiel) {
+    var cardInfo = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var cardList = [];
+    var playerList = [];
+    var players = [1, 2, 3, 4];
+    function inputPairs() {
+        var pairs = prompt("Wie viele Kartenpaare?  Hinweis: Bis 26 Kartenpaare möglich");
+        var sumPair = parseInt(pairs);
+        console.log("inputPairs");
+        return sumPair;
+    }
+    var menge = inputPairs();
+    console.log(menge);
+    function inputPlayers() {
+        var spielerAnzahl = prompt("Wie viele spielen mit? 1 bis 4 Spieler möglich");
+        var sumPlayer = parseInt(spielerAnzahl);
+        console.log("inputPlayers");
+        return sumPlayer;
+    }
+    var output = inputPlayers();
+    console.log(output);
+    function shuffleArray(x) {
+        for (var i = 1; i <= x; i++) {
+            var content = cardInfo[0];
+            cardList.push(content);
+            cardList.push(content);
+            var removed = cardInfo.splice(0, 1);
+        }
+        console.log("Content CardList", cardList);
+    }
+    function showPlayers(x) {
+        for (var i = 0; i <= x; i++) {
+            var inhalt = players[0];
+            playerList.push(inhalt);
+            var removed = players.splice(0, 1);
+            console.log("showPlayers");
+        }
+    }
+    console.log("Content playerList", playerList);
+    console.log("Länge Infolist", cardInfo.length);
+    console.log("Content Infolist", cardInfo);
+    function generateCards(y) {
+        var node = document.getElementById("memoryBoard");
+        var childNodeHTML;
+        var i = 0;
+        while (i < (y * 2)) {
+            var min = 0;
+            var max = (cardList.length);
+            // min = Math.ceil(min);
+            //  max = Math.floor(max);
+            var random = Math.floor(Math.random() * (max - min)) + min;
+            var classRandom = Math.floor(Math.random() * (3 - 0)) + 0;
+            console.log("Card:" + i);
+            console.log(random);
+            childNodeHTML = "<div  class='card' id='Karte" + i + "'>";
+            childNodeHTML += "<p class='info'>";
+            childNodeHTML += cardList[random];
+            childNodeHTML += "</p>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+            console.log("Länge Cardlist nach Generate, " + cardList.length);
+            var content = cardList[random];
+            var removed = cardList.splice(random, 1);
+            console.log(cardList);
+            i++;
+        }
+    }
+    function generatePlayers(x) {
+        var node = document.getElementById("footer");
+        var childNodeHTML;
+        var i = 0;
+        while (i < (playerList.length - 1)) {
+            childNodeHTML = "";
+            childNodeHTML += "<div class='player' id='Spieler" + i + "'>";
+            childNodeHTML += "<p class='playerInfo'>";
+            childNodeHTML += playerList[i];
+            childNodeHTML += ".Spieler</p>";
+            childNodeHTML += "<p class ='punkte'>";
+            childNodeHTML += "Punkte"; //Hier komm dann die Punktenanz
+            childNodeHTML += "</div>";
+            console.log("HTML-text" + childNodeHTML);
+            node.innerHTML += childNodeHTML;
+            var inhaltPlayer = players[output];
+            console.log(players);
+            i++;
+        }
+    }
+    // Hauptprogramm
+    function main() {
+        shuffleArray(menge);
+        showPlayers(output);
+        console.log("main");
+        // Content erzeugen
+        generateCards(menge);
+        generatePlayers(output);
+    }
+    // Add EventListener 
+    document.addEventListener('DOMContentLoaded', main);
+})(memorySpiel || (memorySpiel = {}));
+//# sourceMappingURL=game.js.map
---
+++ Aufgabe2/js/game.ts
@@ -0,0 +1,199 @@
+
+namespace memorySpiel {
+
+
+
+    let cardInfo: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let cardList: string[] = [];
+    let playerList: number[] = [];
+    let players: number[] = [1,2,3,4];
+
+
+
+
+
+
+    function inputPairs(): number {
+
+        let pairs: string = prompt("Wie viele Kartenpaare?  Hinweis: Bis 26 Kartenpaare möglich")
+        let sumPair: number = parseInt(pairs)
+
+
+        console.log("inputPairs")
+
+
+        return sumPair;
+    }
+
+    let menge: number = inputPairs();
+
+    console.log(menge)
+
+
+
+
+
+
+
+
+    function inputPlayers(): number {
+
+        let spielerAnzahl: string = prompt("Wie viele spielen mit? 1 bis 4 Spieler möglich")
+        let sumPlayer: number = parseInt(spielerAnzahl)
+
+        console.log("inputPlayers")
+
+        return sumPlayer;
+    }
+
+    let output: number = inputPlayers();
+
+    console.log(output);
+
+
+
+
+
+    function shuffleArray(x: number): void {
+
+        for (let i: number = 1; i <= x; i++) {
+
+
+            let content: string = cardInfo[0];
+            cardList.push(content);
+            cardList.push(content);
+
+            let removed = cardInfo.splice(0, 1);
+
+        }
+        console.log("Content CardList", cardList);
+
+
+    }
+
+
+
+    function showPlayers(x: number): void {
+
+        for (let i: number = 0; i <= x; i++) {
+
+            let inhalt: number = players[0];
+            playerList.push(inhalt);
+
+           let removed = players.splice(0, 1);
+
+
+            console.log("showPlayers");
+
+        }
+
+    }
+
+    console.log("Content playerList", playerList);
+
+
+
+    console.log("Länge Infolist", cardInfo.length);
+    console.log("Content Infolist", cardInfo);
+
+
+
+
+
+
+
+
+
+
+
+    function generateCards(y: number): void {
+        let node: any = document.getElementById("memoryBoard");
+        let childNodeHTML: string;
+        let i: number = 0;
+
+
+        while (i < (y * 2)) {
+            let min: number = 0;
+            let max: number = (cardList.length);
+            // min = Math.ceil(min);
+            //  max = Math.floor(max);
+            let random: number = Math.floor(Math.random() * (max - min)) + min;
+            var classRandom : number= Math.floor(Math.random() * (3 - 0)) + 0;
+            console.log("Card:" + i);
+            console.log(random);
+
+            childNodeHTML = "<div  class='card' id='Karte" + i + "'>";
+            childNodeHTML += "<p class='info'>";
+            childNodeHTML += cardList[random];
+            childNodeHTML += "</p>";
+            childNodeHTML += " </div> ";
+            node.innerHTML += childNodeHTML;
+            console.log("Länge Cardlist nach Generate, " + cardList.length)
+            var content: string = cardList[random];
+
+            var removed = cardList.splice(random, 1);
+            console.log(cardList);
+
+            i++;
+        }
+    }
+
+
+
+
+    function generatePlayers(x: number): void {
+
+        let node: any = document.getElementById("footer");
+
+        let childNodeHTML: string;
+
+        let i: number = 0;
+
+        while (i < (playerList.length-1) ){
+
+           
+            
+            childNodeHTML = "";
+            childNodeHTML += "<div class='player' id='Spieler" + i + "'>";
+            childNodeHTML += "<p class='playerInfo'>";
+            childNodeHTML += playerList[i];
+            childNodeHTML += ".Spieler</p>";
+            childNodeHTML += "<p class ='punkte'>";
+            childNodeHTML += "Punkte"; //Hier komm dann die Punktenanz
+            childNodeHTML += "</div>";
+            
+
+            console.log("HTML-text" + childNodeHTML);
+
+            node.innerHTML += childNodeHTML;
+
+            let inhaltPlayer: number = players[output];
+
+            console.log(players);
+
+            i++;
+
+        }
+    }
+
+
+    // Hauptprogramm
+    function main(): void {
+
+        shuffleArray(menge);
+        showPlayers(output);
+
+        console.log("main");
+
+        // Content erzeugen
+        generateCards(menge);
+        generatePlayers(output);
+    }
+
+
+
+
+
+    // Add EventListener 
+    document.addEventListener('DOMContentLoaded', main);
+}
