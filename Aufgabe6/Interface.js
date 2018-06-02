0d6ddb73fe41c8a2d48eae0700b59ac5beb6e45b
patrickmk <patrickmk@patricks-mbp-2>
2018-06-02 12:02:09 +0200

    aufgabe 6

---
+++ Aufgabe6/Interface.js
@@ -0,0 +1,6 @@
+var aufgabe6;
+(function (aufgabe6) {
+    // Homogenes assoziatives Array zur Speicherung eines Studierenden unter seiner Matrikelnummer
+    aufgabe6.studiHomoAssoc = {};
+})(aufgabe6 || (aufgabe6 = {}));
+//# sourceMappingURL=Interface.js.map
