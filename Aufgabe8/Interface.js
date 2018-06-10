56d9605efea9adeb3c8b471955b92a80572e3e9a
patrickmk <patrickmk@patricks-mbp-2.eduroam.hs-furtwangen.de>
2018-06-10 13:15:08 +0200

    Aufgabe8 test

---
+++ Aufgabe8/Interface.js
@@ -0,0 +1,6 @@
+var aufgabe8;
+(function (aufgabe8) {
+    // Homogenes assoziatives Array zur Speicherung eines Studierenden unter seiner Matrikelnummer
+    aufgabe8.studiHomoAssoc = {};
+})(aufgabe8 || (aufgabe8 = {}));
+//# sourceMappingURL=Interface.js.map
---
+++ Aufgabe8/Interface.ts
@@ -0,0 +1,21 @@
+namespace aufgabe8 {
+    // Struktur des heterogenen assoziativen Arrays als Datensatz 
+    export interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        studiengang: string;
+    }
+
+    // Struktur des homogenen assoziativen Arrays, bei dem ein Datensatz der Matrikelnummer zugeordnet ist
+    export interface Studis {
+        [matrikel: string]: Studi;
+    }
+
+    // Homogenes assoziatives Array zur Speicherung eines Studierenden unter seiner Matrikelnummer
+    export let studiHomoAssoc: Studis = {};
+}
+
+
